3c3ec5ced5643561f57a984a122f3c0194a7f28a
xiaoye <dlut_liuyuan@163.com>
2023-05-28 13:21:34 +0800

    Update EDSR code.

---
+++ src/data/aorc256_test_self.py
@@ -0,0 +1,29 @@
+import os
+
+from data import common
+from data import srdata_era
+
+import numpy as np
+
+import torch
+import torch.utils.data as data
+
+class aorc256_test_self(srdata_era.SRData):
+    """
+    Define a class to load .npy data for 128 to 512 task
+    """
+
+    def __init__(self, args, name='', train=True, benchmark=True):
+        super(aorc256_test_self, self).__init__(
+            args, name=name, train=train, benchmark=True
+        )
+
+    def _set_filesystem(self, dir_data):
+        self.apath = os.path.join(dir_data, 'aorc_32_256_test_self')
+        self.dir_hr = os.path.join(self.apath, 'hr_256')
+        if self.input_large:
+            self.dir_lr = os.path.join(self.apath, 'lr_32')
+        else:
+            self.dir_lr = os.path.join(self.apath, 'lr_32')
+        self.ext = ('', '.npy')
+
